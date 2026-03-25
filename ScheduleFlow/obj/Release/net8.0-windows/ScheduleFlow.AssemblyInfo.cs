b8a898ccdb0e34591a7f56d8c7799024e8c094ad
Kyra Valiquette <6310411@cegepmv.ca>
2026-03-25 12:48:32 -0400

    AccueilStart

--- ScheduleFlow/obj/Release/net8.0-windows/ScheduleFlow.AssemblyInfo.cs
+++ ScheduleFlow/obj/Release/net8.0-windows/ScheduleFlow.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScheduleFlow")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4fe5204911533e4c9cff9558fc5e6f4ba85390")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScheduleFlow")]
