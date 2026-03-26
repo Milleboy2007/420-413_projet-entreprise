d13fafeb91d71b24166236aad00cd6bccd9e3b55
Milleboy2007 <Nathan2007@videotron.ca>
2026-03-26 17:22:18 -0400

    correctif

--- ScheduleFlow/obj/Debug/net8.0-windows/ScheduleFlow.AssemblyInfo.cs
+++ ScheduleFlow/obj/Debug/net8.0-windows/ScheduleFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScheduleFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a1f4e4fbedeed9ef70a4b3d9a0d08a6f0f82d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+583af68892c1a6893ce11a59fb6d1a21848a54c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScheduleFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScheduleFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
