3525d3bb0a85e504cbcbc3658bf0479d0a20caab
rym31 <rym4085@outlook.com>
2026-03-25 22:50:31 -0400

    Création page acceuil gérant

--- ScheduleFlow/obj/Debug/net8.0-windows/ScheduleFlow.AssemblyInfo.cs
+++ ScheduleFlow/obj/Debug/net8.0-windows/ScheduleFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScheduleFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4fe5204911533e4c9cff9558fc5e6f4ba85390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c0551f0ff425ed380a8b2f567e72fd40741682")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScheduleFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScheduleFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
