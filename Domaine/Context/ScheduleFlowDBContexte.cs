0fd7d2c34c67ff8030c5c99ece20c69fe04100ae
Milleboy2007 <Nathan2007@videotron.ca>
2026-04-11 16:08:06 -0400

    Creation de l'entity et repo des quarts

---
+++ Domaine/Context/ScheduleFlowDBContexte.cs
@@ -0,0 +1,20 @@
+﻿using Domaine.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaine.Context
+{
+    public class ScheduleFlowDBContexte: DbContext
+    {
+        public DbSet<Quart> Quarts { get; set; }
+        
+        public ScheduleFlowDBContexte(DbContextOptions<ScheduleFlowDBContexte> options) : base(options)
+        {
+
+        }
+    }
+}
