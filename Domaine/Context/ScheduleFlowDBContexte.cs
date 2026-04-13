4b6efc4dbc0e5c76254f80790a8d700f5aa4a650
Nitro <2484435@cegepmv.ca>
2026-04-13 11:09:24 -0400

    Combinaison DBContexte

--- Domaine/Context/ScheduleFlowDBContexte.cs
+++ Domaine/Context/ScheduleFlowDBContexte.cs
@@ -10,7 +10,11 @@ namespace Domaine.Context
 {
     public class ScheduleFlowDBContexte: DbContext
     {
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
         public DbSet<Quart> Quarts { get; set; }
+        internal DbSet<FeuilleDispo> FeuilleDispos { get; set; }
+        internal DbSet<CreneauDispo> CreneauDispos { get; set; }
+        public DbSet<DemandeConge> DemandeConges { get; set; }
 
         public ScheduleFlowDBContexte(DbContextOptions<ScheduleFlowDBContexte> options) : base(options)
         {
@@ -19,6 +23,26 @@ namespace Domaine.Context
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
         //    optionsBuilder.UseSqlite("Data Source=ScheduleFlowDB.db");
+        //}
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    string folderPath = Path.Combine(AppContext.BaseDirectory, "Data");
+
+        //    if (!Directory.Exists(folderPath))
+        //    {
+        //        Directory.CreateDirectory(folderPath);
+        //    }
+
+        //    string dbPath = Path.Combine(folderPath, "ScheduleFlow.db");
+        //    optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        //}
+
+        //public override int SaveChanges()
+        //{
+        //    // Crée la BD si elle n'existe pas
+        //    Database.EnsureCreated();
+        //    return base.SaveChanges();
         //}
     }
 }
