9e35b7a5324e735bf68511182541c4eda3ec7067
Ellyn Saint-Firmin <6309487@cegepmv.ca>
2026-05-02 02:40:40 -0400

    AnnonceTable

---
+++ Domaine/Repo/AnnonceRepository.cs
@@ -0,0 +1,49 @@
+﻿using Domaine.Context;
+using Domaine.Interface;
+using Domaine.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaine.Repo
+{
+    internal class AnnonceRepository : IAnnonceRepository
+    {
+        private readonly ScheduleFlowDBContexte _context;
+
+        public AnnonceRepository(ScheduleFlowDBContexte context)
+        {
+            _context = context;
+        }
+        public async Task PublierAnnonce(Annonce annonce)
+        {
+            _context.Add(annonce);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task ModifierAnnonce(Annonce annonce)
+        {
+            _context.Annonces.Update(annonce);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Annonce> RechercherAnnonce(Annonce annonceId)
+        {
+            return await _context.Annonces.FindAsync(annonceId);
+        }
+
+        public async Task SupprimerAnnonce(Annonce annonceID)
+        {
+            var annonce = _context.Annonces.FindAsync(annonceID);
+
+            if(annonce != null)
+            {
+                _context.Annonces.Remove(annonceID);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
