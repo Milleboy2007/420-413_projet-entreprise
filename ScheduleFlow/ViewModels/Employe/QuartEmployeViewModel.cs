92f04c90977896e1642210dc98676bcee1ca09fb
Milleboy2007 <Nathan2007@videotron.ca>
2026-05-03 21:57:56 -0400

    Affichage Quart Dispo employer

---
+++ ScheduleFlow/ViewModels/Employe/QuartEmployeViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Domaine.Entity;
+using Domaine.Interface;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleFlow.ViewModels.Employe
+{
+    public partial class QuartEmployeViewModel : ObservableObject
+    {
+        private readonly IQuartRepository _quartRepo;
+
+        [ObservableProperty]
+        private ObservableCollection<Quart> _quartsDisponibles;
+
+        public QuartEmployeViewModel(IQuartRepository quartRepo)
+        {
+            _quartRepo = quartRepo;
+            ChargerQuarts();
+        }
+
+        private async void ChargerQuarts()
+        {
+            QuartsDisponibles = new ObservableCollection<Quart>(await _quartRepo.GetAllQuartAsync());
+        }
+
+        [RelayCommand]
+        public void DemanderQuart()
+        {
+
+        }
+    }
+}
